5d9aa5dba4b3dfa9ec1ae9bf752fa346dccf4d0d
b14cknc0d3 <b14cknc0d3@gmail.com>
2021-11-13 10:07:29 +0630

    yt admin

--- properties/admin.py
+++ properties/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from properties.models import Bid, Properties
+from properties.models import Bid, Properties, Youtube
 
 # Register your models here.
 admin.site.register(Bid)
 admin.site.register(Properties)
+admin.site.register(Youtube)
