f70b51d0291248667da8c445f62e4e36a455aa7a
b14cknc0d3 <b14cknc0d3@gmail.com>
2021-11-13 10:12:56 +0630

    url path

--- properties/urls.py
+++ properties/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
-from properties.views import BidCreateView, PropertiesCreateView, PropertiesView
+from properties.views import BidCreateView, PropertiesCreateView, PropertiesView, YouTubeView
 
 
 urlpatterns = [
     path("api/properties/",  PropertiesView.as_view(), name="properties_list"),
     path("api/bid/<int:pk>/", BidCreateView.as_view(), name="bid_create"),
-    path("api/properties/create/", PropertiesCreateView.as_view(), name="properties_create")
+    path("api/properties/create/", PropertiesCreateView.as_view(), name="properties_create"),
+    path("api/youtube/", YouTubeView.as_view(), name="yt_list")
 ]
